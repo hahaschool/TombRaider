c9eb62a3e9ee5e96ca96ec793f8eea61a30ab5bc
Adam Chang <hahaschool.wing@gmail.com>
2016-02-12 00:19:45 +0800

    Initial Commit

---
+++ TombRaider/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  TombRaider
+//
+//  Created by 張正昊 on 12/2/2016.
+//  Copyright © 2016 hahaschool. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
