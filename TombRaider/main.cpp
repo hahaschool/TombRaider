71310cb1f34573ecac04d4b6d6125a08049f774b
Adam Chang <hahaschool.wing@gmail.com>
2016-02-17 16:04:11 +0800

    Main Runloop

--- TombRaider/main.cpp
+++ TombRaider/main.cpp
@@ -7,28 +7,101 @@
 //
 
 #include <iostream>
-#include "TRPathFinder.hpp"
+#include <SDL2/SDL.h>
+#include <SDL2_image/SDL_image.h>
+#include <SDL2_ttf/SDL_ttf.h>
+#include "TRGameController.hpp"
 
-int main(int argc, const char * argv[]) {
-    int n,m,a,b,c,d;
-    std::cin >> n >> m >> a >> b >> c >> d;
-    TRPathFinder pf;
-    pf.init(n, m, 3);
-    pf.setHeuristic(TRPathFinderHeuristicManhatton);
-    pf.setStartingPoint(b, a);
-    pf.setTargetPoint(d, c);
-    for(int i = 0;i < n; i++){
-        for(int j = 0;j < m; j++){
-            char q;
-            scanf(" %c",&q);
-            if(q == '0'){
-                pf.setMatrix(j, i, TRPathFinderMatrixObstacle);
+const int SCREEN_WIDTH = 800;
+const int SCREEN_HEIGHT = 600;
+
+SDL_Window *gWindow = NULL;
+SDL_Renderer *gRenderer = NULL;
+TRPathFinder *gPathFinder = NULL;
+TRGrider *gGrider = NULL;
+TRGameController *gGameController = NULL;
+
+
+bool init(){
+    //初始化成功标志
+    bool success = true;
+    
+    gGameController = new TRGameController;
+    
+    gGrider = new TRGrider;
+    
+    gPathFinder = new TRPathFinder;
+    
+    
+    
+    if(SDL_Init(SDL_INIT_EVERYTHING) < 0){
+        printf("Error: SDL_Init Failed. SDL Error: %s\n",SDL_GetError());
+        success = false;
+    }else{
+        //线性贴图过滤
+        if (!SDL_SetHint(SDL_HINT_RENDER_SCALE_QUALITY, "1")) {
+            printf("Warning: Linear texture filtering not enabled!");
+        }
+        //创建窗口
+        gWindow = SDL_CreateWindow("SDL Tutorial", SDL_WINDOWPOS_UNDEFINED, SDL_WINDOWPOS_UNDEFINED, SCREEN_WIDTH, SCREEN_HEIGHT, SDL_WINDOW_SHOWN);
+        if(gWindow == NULL){
+            printf("Error:Window creation failed. SDL_Error: %s\n",SDL_GetError());
+            success = false;
+        }else{
+            //垂直同步
+            gRenderer = SDL_CreateRenderer(gWindow, -1, SDL_RENDERER_ACCELERATED | SDL_RENDERER_PRESENTVSYNC);
+            if(gRenderer == NULL){
+                printf("Error:Render Creation Failed.SDL_Error : %s\n",SDL_GetError());
+                success = false;
             }else{
-                pf.setMatrix(j, i, TRPathFinderMatrixRoad);
+                //设定渲染器默认颜色
+                SDL_SetRenderDrawColor(gRenderer, 0xFF, 0xFF, 0xFF, 0xFF);
+                //初始化PNG载入
+                int imgFlag = IMG_INIT_PNG;
+                if(!(IMG_Init(imgFlag) & imgFlag)){
+                    printf("Error:SDL_image init failed.SDL_image Error: %s\n",IMG_GetError());
+                    success = false;
+                }
             }
         }
     }
-    pf.findPath();
-    pf.printOut();
+    return success;
+}
+
+void close(){
+    //Destroy window
+    SDL_DestroyRenderer(gRenderer);
+    SDL_DestroyWindow(gWindow);
+    gWindow = NULL;
+    gRenderer = NULL;
+    
+    //Quit SDL subsystem
+    IMG_Quit();
+    SDL_Quit();
+}
+
+int main(int argc, const char * argv[]) {
+    if (!init()) {
+        printf("Error:Failed to load.\n");
+    }else{
+        bool quit = false;
+        SDL_Event e;
+        
+        while(!quit){
+            while (SDL_PollEvent(&e) != 0) {
+                if(e.type == SDL_QUIT){
+                    quit = true;
+                }
+                //handleEvent
+                
+                
+                
+                
+            }
+            
+            
+        }
+    }
+    colse();
     return 0;
 }
